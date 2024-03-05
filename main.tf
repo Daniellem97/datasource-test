9e83ef79964c3abff14b39d84562b841e20df719
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-05 08:34:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
-data "spacelift_github_enterprise_integration" "this" {
-  id = "legacy-test" # Optional: Specify the integration ID if not using the default
+data "spacelift_gitlab_integration" "this" {
+  id = "legacygitlabtest" 
 }
 
 # Define the stack in Spacelift
-resource "spacelift_stack" "metabase_test" {
-  name              = "metabase-test"
-  repository        = "tftest"
+resource "spacelift_stack" "testing" {
+  name              = "gitlab-test"
+  repository        = "test"
   branch            = "main"
-  project_root      = "path/to/metabase"
   terraform_version = "1.5.7"
-  space_id = "metabasetest2-01HR4MYZWE0APZGRDR21CCDB45"
+  space_id = "metabasetest-01HBRT2GZTJEVVA4APJDNTNSV1"
   # GitHub Enterprise settings for the stack
-  github_enterprise {
-    namespace = "Daniellem97" # The GitHub organization or user the repository belongs to
-    id = data.spacelift_github_enterprise_integration.this.id
+  gitlab {
+    namespace = "Daniellem97"
+    id = data.spacelift_gitlab_integration.this.id
   }
 }
